0e7ef87dc5bfeaba94d32b8c8bced986a98338d7
Abhay Rana <officialabhi730@gmail.com>
2025-05-21 21:30:36 +0530

    __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
 # This file is intentionally left empty to mark this directory as a Python package
-from .utils import load_svg, preprocess_text,extract_section_numbers
+from .utils import load_svg, preprocess_text, extract_section_numbers, calculate_similarity
